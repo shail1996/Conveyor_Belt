61370918fa3c1c77aada6f254d040959f161d3b6
Shail <shail@mersus.ie>
2021-03-25 23:27:06 +0000

    Initial Commit

---
+++ Assets/Scripts/CreatingObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatingObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] cubes;
+    [SerializeField] Transform initialPosition;
+    
+    // Random number variables
+    private float randomFloat;
+    private int randomInt;
+
+    // Time variables
+    private float time, delayTime;
+
+    void Start()
+    {
+        time = 0.0f;
+        delayTime = 2.5f;
+        initializePrefab();
+    }
+
+    void Update()
+    {
+        time = time + 1.0f * Time.deltaTime;
+
+        if (time >= delayTime && delayTime > 0.2f)
+        {
+            delayTime -= 0.1f;
+            time = 0.0f;
+            initializePrefab();
+        }
+
+    }
+
+    private void initializePrefab()
+    {
+        randomFloat = Random.Range(-0.5f, 0.5f);
+        randomInt = Random.Range(0, 2);
+
+        Instantiate(cubes[randomInt], new Vector3(initialPosition.position.x, initialPosition.position.y, randomFloat), initialPosition.rotation);
+    }
+}
---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    private float speed = 0.01f;
+
+    void Update()
+    {
+        this.gameObject.transform.Translate(speed, 0, 0);
+        if (this.gameObject.transform.position.x > 7)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            //Debug.Log(this.gameObject.name);
+            if(this.gameObject.name == "Blue(Clone)" && this.gameObject.transform.position.x > 2.5 && this.gameObject.transform.position.x < 5)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
